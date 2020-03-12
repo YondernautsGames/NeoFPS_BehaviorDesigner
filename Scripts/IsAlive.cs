c1f65da84df8db1a635b002c79eb97b6712f8e13
Ross Gardler <ross.gardler@microsoft.com>
2020-03-11 20:42:42 -0700

    Initial (basic) Behavior Designer integration (#1)
    * Initial scene
    * basic behavior tree
    * If red touches the player then the player takes damage (DamageSource task)
    * Red takes damage from player
    * IsAlive conditional

---
+++ Scripts/IsAlive.cs
@@ -0,0 +1,46 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using NeoFPS;
+using UnityEngine;
+
+namespace BehaviorDesigner
+{
+    [TaskCategory("Neo FPS")]
+    [TaskDescription("Test if a character is alive or not. Returning success if alive.")]
+    [TaskName("Is Alive")]
+    public class IsAlive : Conditional
+    {
+        [Runtime.Tasks.Tooltip("The object that we want to test for an alive status. Must have an IHealthManager attached. If set to null then the task owner will be used.")]
+        public SharedGameObject targetGameObject;
+
+        private IHealthManager healthManager;
+        private GameObject prevGameObject;
+
+        public override void OnStart()
+        {
+            GameObject currentGameObject = GetDefaultGameObject(targetGameObject.Value);
+            if (currentGameObject != prevGameObject)
+            {
+                if (currentGameObject != null)
+                {
+                    healthManager = currentGameObject.GetComponent<IHealthManager>();
+                } else
+                {
+                    healthManager = null;
+                }
+            }
+        }
+
+        public override TaskStatus OnUpdate()
+        {   
+            if (healthManager != null && healthManager.isAlive)
+            {
+                return TaskStatus.Success;
+            }
+            else
+            {
+                return TaskStatus.Failure;
+            }
+        }
+    }
+}
