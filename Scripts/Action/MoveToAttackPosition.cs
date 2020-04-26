6d1e0ef7d1ffd53a253859f052d785abdbd62b33
Ross Gardler <ross.gardler@microsoft.com>
2020-04-25 19:16:17 -0800

    Now we start to get real AI, though it is still extremely basic. The enemy will now search for the player using a very naive algorithm (basically move forwards at +/-35 degrees intil the player is found. Once sighted he will move into a position where he can attack the player (i.e. get within range) and fire upon him. The get into position algorithm is naive too (basically, walk directly towards the player). Next up start creating some more intelligence in the move algorithms.

--- Scripts/Action/MoveToAttackPosition.cs
+++ Scripts/Action/MoveToAttackPosition.cs
@@ -10,67 +10,39 @@ namespace NeoFPS.BehaviourDesigner
 	[TaskCategory("Neo FPS")]
 	[TaskDescription("Move to a suitable position to attack the player. By default the agent will attempt to find a suitable location 75% of the way between the min and max attack distances.")]
 	[TaskName("Move to Attack Position")]
-	public class MoveToAttackPosition : BehaviorDesigner.Runtime.Tasks.Action
+	public class MoveToAttackPosition : AbstractMovementAction
 	{
-		[BehaviorDesigner.Runtime.Tasks.Tooltip("The object to move, if null then the owning object will be used.")]
-		public SharedGameObject m_ActionTarget;
-		[BehaviorDesigner.Runtime.Tasks.Tooltip("The target to attack.")]
-		public SharedGameObject m_AttackTarget;
 		[BehaviorDesigner.Runtime.Tasks.Tooltip("The minimum distance from which to attack.")]
 		public SharedFloat m_MinimumAttackDistance = 5;
 		[BehaviorDesigner.Runtime.Tasks.Tooltip("The maximum distance from which to attack.")]
 		public SharedFloat m_MaximumAttackDistance = 25;
-		[BehaviorDesigner.Runtime.Tasks.Tooltip("How often will the agent reconsider its current movement plan?")]
-		public SharedFloat m_EvaluationFrequency = 2;
 
-		private GameObject prevGameObject;
-		private NavMeshAgent m_Agent;
-		private float m_NextEvaluationTime = 0;
-		private Vector3 m_GoalPosition;
-
-		public override void OnStart()
+		internal override bool SetOptimalNextPosition()
 		{
-			var currentGameObject = GetDefaultGameObject(m_ActionTarget.Value);
-			if (currentGameObject != prevGameObject)
-			{
-				m_Agent = currentGameObject.GetComponent<NavMeshAgent>();
-				prevGameObject = currentGameObject;
-				m_NextEvaluationTime = 0;
-			}
-		}
+			Debug.LogWarning("MoveToAttackPosition.SetOptimalNextPosition is just finding a point on a straight line between AI and player. This is too simplistic.");
+			Vector3 targetPos = m_EnemyTarget.Value.transform.position;
+			float distance = Vector3.Distance(m_EnemyTarget.Value.transform.position, transform.position);
 
-		public override TaskStatus OnUpdate()
-		{
-			if (Time.realtimeSinceStartup < m_NextEvaluationTime) return TaskStatus.Failure;
+            if (distance >= m_MinimumAttackDistance.Value && distance <= m_MaximumAttackDistance.Value)
+            {
+                return false;
+            }
 
-			m_GoalPosition = ScanForOptimalPosition();
-			Debug.Log("Optimal position from which to attck is " + m_GoalPosition);
-			m_NextEvaluationTime = Time.realtimeSinceStartup + m_EvaluationFrequency.Value;
-			return m_Agent.SetDestination(m_GoalPosition) ? TaskStatus.Success : TaskStatus.Failure;
-		}
-
-		/// <summary>
-		/// Scans the environmet for an optimal position from which to attack.
-		/// </summary>
-		/// <returns>The position that the agent should try to reach.</returns>
-		private Vector3 ScanForOptimalPosition()
-		{
-			Debug.LogWarning("ScanForOptimalPosition is just finding a point on a straight line between AI and player. This is too simplistic.");
-			Vector3 targetPos = m_AttackTarget.Value.transform.position;
-			Vector3 direction = (targetPos - transform.position).normalized;
-			float distance = Vector3.Distance(targetPos, transform.position);
-			float optimalDistance = m_MinimumAttackDistance.Value;
+            Vector3 direction = (targetPos - transform.position).normalized;
+            float optimalDistance = m_MinimumAttackDistance.Value;
 			optimalDistance += distance > m_MaximumAttackDistance.Value ? (m_MaximumAttackDistance.Value - m_MinimumAttackDistance.Value) * 0.75f : (distance - m_MinimumAttackDistance.Value) * 0.75f;
 
-			return transform.position + (direction * optimalDistance);
-		}
+            Vector3 newPosition = transform.position + (direction * optimalDistance);
+            m_NextEvaluationTime = Time.realtimeSinceStartup + (m_MinEvaluationFrequency.Value * (optimalDistance / distance));
 
-		public override void OnDrawGizmos()
-		{
-			base.OnDrawGizmos();
-
-			Gizmos.color = Color.black;
-			Gizmos.DrawWireSphere(m_GoalPosition, 0.3f);
+            if (GoalPosition != newPosition)
+            {
+                GoalPosition = newPosition;
+                return true;
+            } else
+            {
+                return false;
+            }
 		}
 	}
 }
