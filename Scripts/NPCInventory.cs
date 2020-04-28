19dfa18c2c1d251e21693a6ecdc782e1b8d7ff3a
Ross Gardler <rgardler@apache.org>
2020-04-27 23:48:17 -0700

    Add NPC Inventory, remove mount point script (inventory handles it).

---
+++ Scripts/NPCInventory.cs
@@ -0,0 +1,12 @@
+﻿using NeoFPS.Constants;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace NeoFPS.BehaviourDesigner
+{
+    public class NPCInventory : FpsInventoryQuickSwitch
+    {
+    }
+}
